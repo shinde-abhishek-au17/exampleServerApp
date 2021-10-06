23a0b850821fd2ba80764350e089739b1b9f5bf8
abhishek <abhishekshinde61@gmail.com>
2021-10-06 17:03:53 +0530

    added html and meme

--- serverabhi.js
+++ serverabhi.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req,res) =>{
-
-    res.send(`server working.this is homepage`)
+    res.sendFile(`${__dirname}/index.html`)
 })
 
 console.log(`heroku port number is ${process.env.PORT }`)
