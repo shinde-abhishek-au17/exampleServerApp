774942ad95ebd400ed8d14b9a7229916b8d3f019
abhishek <abhishekshinde61@gmail.com>
2021-10-06 16:38:14 +0530

    updated port value again

--- serverabhi.js
+++ serverabhi.js
@@ -5,13 +5,12 @@ const app = express()
 app.get('/',(req,res) =>{
 
     res.send(`server working.this is homepage`)
-
-
 })
 
-console.log.apply(`heroku port number is ${process.env.PORT }`)
-const PORT = process.env.PORT || 3000;
+console.log(`heroku port number is ${process.env.PORT }`)
 
-app.listen(PORT, () =>{
-    console.log(`server started at port ${PORT}`)
+const prt = process.env.PORT || 3000;
+
+app.listen(prt, () =>{
+    console.log(`server started at port ${prt}`)
 })
